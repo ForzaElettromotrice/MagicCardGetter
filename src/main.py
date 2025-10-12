bdeb4129625291a5ae5273a29006ae7e6f387e0c
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-10-12 18:55:16 +0200

    Ora se la carta non esiste in italiano, va a vedere tutte le versioni e cerca se ne esiste una in italiano

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
 import re
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = "https://gatherer.wizards.com"
+WIZARDS_GATHERER_BASE_URL = "https://gatherer.wizards.com"
+SCRYFALL_BASE_URL = "https://api.scryfall.com"
 FILE_PATH = "cards.txt"
 OUTPUT_DIR = "out_images"
 
@@ -18,14 +19,14 @@ def parse_line(line: str) -> Dict[str, str | int]:
         code_set, number_set = number_set.split()[0].split("-")
     else:
         number_set = number_set.split()[0]
-    return { "n": int(n), "name": re.sub(r'[^a-z ]+', '', name.strip().lower()), "code_set": code_set, "number_set": int(number_set) }
+    return { "n": int(n), "name": re.sub(r'[^a-z ]+', '', name.strip()), "code_set": code_set, "number_set": number_set }
 def read_cards() -> List[Dict[str, str | int]]:
     with open(FILE_PATH) as f:
         cards = [parse_line(line) for line in f]
     return cards
 
-def find_card_url(name: str, code_set: str, number_set: int) -> str:
-    return f"{BASE_URL}/{code_set}/it-it/{number_set}/{name.replace(" ", "-")}"
+def find_card_url(name: str, code_set: str, number_set: str) -> str:
+    return f"{WIZARDS_GATHERER_BASE_URL}/{code_set.upper()}/it-it/{number_set}/{name.replace(" ", "-").lower()}"
 def get_image_url(url: str) -> str:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -34,6 +35,21 @@ def get_image_url(url: str) -> str:
     for img in imgs:
         if img.has_attr("data-testid") and img["data-testid"] == "cardFrontImage":
             return img.get("src")
+
+    return ""
+def get_versions(name: str) -> List[Tuple[str, str]]:
+    params = { "exact": name }
+    response = requests.get(f"{SCRYFALL_BASE_URL}/cards/named", params = params)
+    response = requests.get(response.json()["prints_search_uri"])
+    cards = response.json()["data"]
+    return [(card["set"], card["collector_number"]) for card in cards]
+def get_another_image_url(name: str) -> str:
+    versions = get_versions(name)
+    for set_, number in versions:
+        url = find_card_url(name, set_, number)
+        image_url = get_image_url(url)
+        if image_url != "":
+            return image_url
     return ""
 
 def save_image(url: str, n: int, name: str):
@@ -53,14 +69,16 @@ def save_image(url: str, n: int, name: str):
 
 def main():
     cards = read_cards()
-
+    
     for diz in cards:
         n, name, code_set, number_set = diz.values()
         url = find_card_url(name, code_set, number_set)
         image_url = get_image_url(url)
         if image_url == "":
-            print(f"Card {name} not found")
-            continue
+            image_url = get_another_image_url(name)
+            if image_url == "":
+                print(f"Card {name} not found")
+                continue
         save_image(image_url, n, name)
         print(f"Card {name} saved")
 
